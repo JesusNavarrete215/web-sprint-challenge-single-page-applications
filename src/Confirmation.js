d89a5d537a07a1fb358edd4ee9e64eef7cb7d755
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-08-20 16:46:20 -0700

    still working

---
+++ src/Confirmation.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Confirmation(props) {
+  return (
+    <div>
+      <img src="https://media.istockphoto.com/photos/fresh-italian-pizza-is-ready-to-bake-in-hot-oven-picture-id1255786223?s=612x612" />
+      <h1> Your Pizza is Being Baked </h1>
+    </div>
+  );
+}
--- src/Form.js
+++ src/Form.js
@@ -1,45 +1,27 @@
 import React, { useState } from "react";
 
-export default function Form() {
-  return (
-    <div>
-      <h2>Build Your Own Pizza</h2>
-      <div>
-        <h2>Choice of Size</h2>
-        <h3>Required/.</h3>
-      </div>
-      <div>
-        <select>
-          <option value="">Select</option>
-          <option value="twelve">12"</option>
-          <option value="fourteen">14"</option>
-          <option value="sixteen">16"</option>
-        </select>
-      </div>
-      <div>
-        <h3>Choice of Sauce</h3>
-        <h4>Required.</h4>
-        <label>
-          Original Read
-          <input type="checkbox" name="original red" />
-        </label>
-        <label>
-          Garlic Ranch
-          <input type="checkbox" name="garlic Ranch" />
-        </label>
-        <label>
-          BBQ Sauce
-          <input type="checkbox" name="bbq sauce" />
-        </label>
-        <label>
-          Spinach Alfredo
-          <input type="checkbox" name="spinach alfredo" />
-        </label>
-      </div>
+export default function Form(props) {
 
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+    const [form, setForm] = useState({
+        size: '',
+        sauce: '',
+        topping1: '',
+        topping2: '',
+        special: ''
+    })
+
+  return (
+ <div>
+     <img 
+     src='https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg'/>
+     <div>
+         <select>
+             <option>12"</option>
+             <option>14"</option>
+             <option>16"</option>
+         </select>
+         <button onChange={}>Create My Pizza!</button>
+     </div>
+ </div>
   );
 }
