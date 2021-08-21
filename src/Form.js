cce85c32e03683b1b0f6aaff0d38e3388a6abcb5
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-08-20 17:11:09 -0700

    still working

--- src/Form.js
+++ src/Form.js
@@ -14,11 +14,47 @@ export default function Form() {
     <div>
       <img src="https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg" />
       <div>
-        <select>
-          <option>12"</option>
-          <option>14"</option>
-          <option>16"</option>
-        </select>
+        <label>
+          Choose Size
+          <select>
+            <option>12"</option>
+            <option>14"</option>
+            <option>16"</option>
+          </select>
+        </label>
+
+        <label>
+          Choose Sauce
+          <select>
+            <option>12"</option>
+            <option>14"</option>
+            <option>16"</option>
+          </select>
+        </label>
+
+        <label>
+          Choose Topping
+          <select>
+            <option>12"</option>
+            <option>14"</option>
+            <option>16"</option>
+          </select>
+        </label>
+
+        <label>
+          Choose Topping
+          <select>
+            <option value="12">12"</option>
+            <option value="14">14"</option>
+            <option value="16">16"</option>
+          </select>
+        </label>
+
+        <label>
+          Special
+          <input name="special" value={formValues.special} type="text" />
+        </label>
+
         <button>Create My Pizza!</button>
       </div>
     </div>
