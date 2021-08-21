6af2931adb93a9e4b3e10f7ccdb4d58fb9278af7
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-08-20 17:03:25 -0700

    still working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Route, Link, Switch } from "react-router-dom";
-// import Form from "./Form";
+import Form from "./Form";
 // import HomePage from "./HomePage";
 // import Confirmation from "./Confirmation";
-
-const initialFormValues = {
-  name: "",
-  size: "",
-  topping1: "",
-  topping2: "",
-  special: "",
-};
 
 const App = () => {
   return (
@@ -20,12 +12,15 @@ const App = () => {
         <div className="nav-links">
           {/* 👉 STEP 3 - Make Links to navigate us Home (`/`) and Shop (`/items-list`) */}
           <Link to="/">Home</Link>
+          <Link to="/create-pizza">Create Pizza</Link>
         </div>
       </nav>
       {/*setting up the route and switch */}
 
       <Switch>
-        <Route path="/create-pizza">{/* <Form /> */}</Route>
+        <Route path="/create-pizza">
+          <Form />
+        </Route>
 
         <Route path="/">{/* <HomePage /> */}</Route>
       </Switch>
--- src/Form.js
+++ src/Form.js
@@ -1,27 +1,26 @@
 import React, { useState } from "react";
 
-export default function Form(props) {
+const initialFormValues = {
+  size: "",
+  sauce: "",
+  topping1: "",
+  topping2: "",
+  special: "",
+};
 
-    const [form, setForm] = useState({
-        size: '',
-        sauce: '',
-        topping1: '',
-        topping2: '',
-        special: ''
-    })
-
+export default function Form() {
+  const [formValues, setFormValues] = useState(initialFormValues);
   return (
- <div>
-     <img 
-     src='https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg'/>
-     <div>
-         <select>
-             <option>12"</option>
-             <option>14"</option>
-             <option>16"</option>
-         </select>
-         <button onChange={}>Create My Pizza!</button>
-     </div>
- </div>
+    <div>
+      <img src="https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg" />
+      <div>
+        <select>
+          <option>12"</option>
+          <option>14"</option>
+          <option>16"</option>
+        </select>
+        <button>Create My Pizza!</button>
+      </div>
+    </div>
   );
 }
